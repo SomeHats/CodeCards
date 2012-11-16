90658b48a8e7f9ef2057b70bd1a93eebb1a999d2
Joe Dytrych <me@jdytrych.co.uk>
2012-11-16 10:30:45 +0000

    Update app/assets/CodeCards-data.js

--- app/assets/CodeCards-data.js
+++ app/assets/CodeCards-data.js
@@ -1,5 +1,5 @@
 define.resources({
-	root: "http://codecards.decoded.co/mission-repo",
+	root: "http://localhost:3000",
 	missions: {
 		"sample": {
 			name: "Sample",
@@ -10,10 +10,12 @@ define.resources({
 		"fox": {
 			name: "Fox",
 			description: "Help Fox get as much cake as possible with your awesome programming skills!",
-			source: "/fox/fox.mission.js",
+			source: "/fox/fox.js",
 			style: "/fox/fox.css",
-			template: "/fox/fox.hbs"
+			template: "/fox/template.js"
 		}
 	},
-	languages: ["/languages/alphabet.js"]
+	languages: [
+		"/languages/alphabet.js", 
+		"/languages/fox-lang.js"]
 });
